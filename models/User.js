e62f9dc64e2abdf85dfac43454618cbd831f1245
LAPTOP-JBSIO3B7\ss398 <ss3980@khu.ac.kr>
2020-08-11 00:56:57 +0900

    암호화해서 db저장

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: { // token 유효기간 
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){ //save하기 전에 수행할 func
+    var user = this;
+    
+    if(user.isModified('password')){ //db의 password가 변경될 때만 암호화
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            // Store hash in your password DB, 첫번째 arg가 암호화 되기 전 비밀번호
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
